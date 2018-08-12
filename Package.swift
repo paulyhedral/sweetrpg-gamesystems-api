ba1d8af7794b0172c73f5c83033e1b38f573faf2
Paul Schifferer <paul@schifferers.net>
2018-08-11 22:49:57 -0700

    Checkpoint.

--- Package.swift
+++ Package.swift
@@ -1,25 +1,28 @@
 // swift-tools-version:4.0
 import PackageDescription
 
+
 let package = Package(
-    name: "sweetrpg-gamesystem-api",
+    name: "SweetRPGGameSystemAPI",
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        
+
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
+
         // 👤 Authentication and Authorization layer for Fluent.
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
 
         // JSON-API
-//        .package(url: "https://github.com/vapor-tools/vapor-jsonapi.git", .branch("master"))
+    //    .package(url: "https://github.com/vapor-tools/vapor-jsonapi.git", .branch("master")),
+
+        // Model
+        .package(url: "ssh://git@github.com/paulyhedral/sweetrpg-gamesystem-model.git", .branch("develop")),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Authentication", "FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "GameSystemModel", "Authentication"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
-
